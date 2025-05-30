945e4832ed5213b9971cc4ed213af4e97a20708c
ubparviz <ubparviz@gmail.com>
2025-05-30 06:02:03 +0500

    Bajarildi

--- task13.py
+++ task13.py
@@ -1,6 +1,6 @@
 # Ikkita matn kiritiladi birinchisi ikkinchisida bor yoki yo'qligini aniqlash
 
-text1 = input("Birinchi so'zni kiritng: ")
-text2 = input("Ikkinchi so'zni kiriting: ")
+text1 = str.upper(input("Birinchi so'zni kiritng: "))
+text2 = str.upper(input("Ikkinchi so'zni kiriting: "))
 
 print(text1 in text2)
--- task14.py
+++ task14.py
@@ -1,6 +1,6 @@
 # Ikkita matn o'qilganda bir qil yoki yo'qligini tekshiring
 
-text1 = input("Birinchi so'zni kiritng: ")
-text2 = input("Ikkinchi so'zni kiriting: ")
+text1 = str.upper(input("Birinchi so'zni kiritng: "))
+text2 = str.upper(input("Ikkinchi so'zni kiriting: "))
 
 print(text1 == text2)
