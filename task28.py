f0ec5b8904da659db08ee41eeed60c17326246ca
ubparviz <ubparviz@gmail.com>
2025-05-30 08:27:43 +0500

    Bajarildi

--- task28.py
+++ task28.py
@@ -1,7 +1,7 @@
 # Izoh matnidan “yomon” so‘zlarni olib tashlang, so‘ngra tekshiring
 
 text = str.upper(input("Izoh kiriting: "))
-yomon_soz = "bad"
+yomon_soz = "BAD"
 bormi = yomon_soz in text
 
 print(bormi)
